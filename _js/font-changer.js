76f0a443e05ec73d5b7359c1f68500e43f8d65f5
april@gallaty.com <april@gallaty.com>
2014-01-30 15:25:37 -0800

    website files

---
+++ _js/font-changer.js
@@ -0,0 +1,14 @@
+function smallFont()
+{
+	document.getElementsByTagName("p").style.fontSize = 10;
+}
+
+function mediumFont()
+{
+	document.getElementsByTagName("p").style.fontSize = 14;
+}
+
+function largeFont()
+{
+	document.getElementsByTagName("p").style.fontSize = 18;
+}
